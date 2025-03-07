d22a9bb2182a6338148e2e88391ef12e49831cdf
DHRUVI576 <21SE02IE054@ppsu.ac.in>
2025-03-07 10:42:36 +0000

    datatype or variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 144553
+let accountEmail = "dhruvi@gmail.com"
+var accountPassword = "12345" 
+accountCity = "Jaipur"
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "bangaluru"
+
+
+console.log(accountId);
+
+/*
+prefer not to use var
+becausemof issuenin block scop and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+
+
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs,not browser
+
+console.log(3+3) // code readability shoud be high 
+
+console.log("dhruvi")
+
+let name = "dhruvi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => to power 53
+// bigint 
+// sting => ""
+// boolean => t/f
+// null => standalone value
+// undifined =>
+// symbol =>unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
